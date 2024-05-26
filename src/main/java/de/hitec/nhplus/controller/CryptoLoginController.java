24ad7f798536485a8ec1b672cd8152b3532f79a7
lomberd2 <joeyblankenhagen@gmail.com>
2024-05-26 13:22:07 +0200

    implemented proper errors

--- src/main/java/de/hitec/nhplus/controller/CryptoLoginController.java
+++ src/main/java/de/hitec/nhplus/controller/CryptoLoginController.java
@@ -3,11 +3,17 @@ package de.hitec.nhplus.controller;
 import de.hitec.nhplus.Main;
 import de.hitec.nhplus.datastorage.CryptoUtils;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Window;
 
 public class CryptoLoginController {
+
+    @FXML
+    protected VBox MainVBox;
 
     @FXML
     protected PasswordField passwordBox;
@@ -17,6 +23,8 @@ public class CryptoLoginController {
 
     @FXML
     protected Label labelTag;
+    @FXML
+    protected Label TitleLabel;
 
     @FXML
     protected void handleSubmitButton() {
@@ -33,18 +41,39 @@ public class CryptoLoginController {
             Main.mainWindow();
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
+
+            // Msg box
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Error");
+            alert.setContentText("Error: " + e.getMessage());
+            alert.showAndWait();
+
+            // Clear password field
+            passwordBox.clear();
         }
 
     }
 
+    protected void setupLoginTexts() {
+        TitleLabel.setText("NHPlus - Crypto Login");
+        labelTag.setText("Bitte geben Sie Ihr Passwort ein:");
+        submitButton.setText("Login");
+    }
+
+    protected void setupSetupTexts() {
+        labelTag.setText("Setzen Sie hier Ihr Passwort:");
+        TitleLabel.setText("NHPlus - Crypto Setup");
+        submitButton.setText("Setup DB Encryption");
+    }
+
     @FXML
     public void initialize() {
-        labelTag.setText("Enter your password");
-
-        if (CryptoUtils.isDBEncrypted()) {
-            labelTag.setText("Enter your password");
+        if (!CryptoUtils.isDBEncrypted()) {
+            setupSetupTexts();
         } else {
-            labelTag.setText("Set your password");
+
+            setupLoginTexts();
         }
     }
 }
